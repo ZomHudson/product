89ae7af1ba177d605114850fa841dc6dc7022f1d
ZomHudson <140999294+ZomHudson@users.noreply.github.com>
2025-12-12 04:20:03 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+# Add parent directory to path to import app
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import app
+
+# This is required for Vercel
+handler = app
